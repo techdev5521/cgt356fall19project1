846aaf907ae8dcf9bd4fc55cd2a36bb73d394a15
Justin Campbell <campbell.justin.tyler@gmail.com>
2019-10-31 17:21:18 -0400

    Added skeleton

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="em-US">
+	
+	<head>
+		<title>Project 1: Home</title>
+
+		<!-- Metadata-->
+		<?php include('includes/meta.html'); ?>
+
+		<!-- CSS -->
+		<?php include('includes/css.html'); ?>
+
+		<!-- PHP Libraries -->
+		<?php include('includes/phpLibs.html'); ?>
+
+	</head>
+
+	<body>
+		
+		<header>
+			<!-- Navigation -->
+			<?php include('includes/navigation.html'); ?>
+		</header>
+
+
+
+		<main>
+			
+		</main>
+
+
+
+		<footer>
+			
+		</footer>
+
+
+
+		<!-- Bootstrap JavaScript -->
+		<?php include('includes/bootstrap_javascript.html') ?>
+
+	</body>
+
+
+</html>
