d29e2c8ad840508f8ad4ec215be6ce98e50ac222
Justin Campbell <campbell.justin.tyler@gmail.com>
2019-11-04 02:12:00 -0500

    Modified indentations

--- login.php
+++ login.php
@@ -1,59 +1,61 @@
 <!DOCTYPE html>
 <html lang="em-US">
 
-<head>
-	<title>Login</title>
+	<head>
+		<title>Login</title>
 
-	<!-- Metadata-->
-	<?php include('includes/meta.html'); ?>
+		<!-- Metadata-->
+		<?php include('includes/meta.html'); ?>
 
-	<!-- CSS -->
-	<?php include('includes/css.html'); ?>
+		<!-- CSS -->
+		<?php include('includes/css.html'); ?>
 
-	<!-- PHP Libraries -->
-	<?php include('includes/phpLibs.html'); ?>
+		<!-- PHP Libraries -->
+		<?php include('includes/phpLibs.html'); ?>
 
-</head>
+	</head>
 
-<body>
+	<body>
 
-<header>
-	<!-- Navigation -->
-	<?php include('includes/logo_nav.html'); ?>
-</header>
+	<header>
+		<!-- Navigation -->
+		<?php include('includes/logo_nav.html'); ?>
+	</header>
 
-<div class="container">
-	<div class="row justify-content-center">
-		<div class="col-md-4 my-5">
-			<div class="card my-5 box-shadow px-3 py-4">
-				<form class="form-signin">
-					<h2 class="text-center">Please Login</h2>
-					
-					<div class="form-group my-4">
-						<input type="text" class="form-control" id="username" name="username" placeholder="Username" required>
+	<main>
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-md-4 my-5">
+					<div class="card my-5 box-shadow px-3 py-4">
+						<form class="form-signin">
+							<h2 class="text-center">Please Login</h2>
+							
+							<div class="form-group my-4">
+								<input type="text" class="form-control" id="username" name="username" placeholder="Username" required>
+							</div>
+
+							<div class="form-group my-4">
+								<input type="password" class="form-control" id="password" name="password" placeholder="Password" required>
+							</div>
+
+							<button class="btn btn-lg btn-success btn-block" type="submit">Login</button>
+						</form>
 					</div>
-
-					<div class="form-group my-4">
-						<input type="password" class="form-control" id="password" name="password" placeholder="Password" required>
-					</div>
-
-					<button class="btn btn-lg btn-success btn-block" type="submit">Login</button>
-				</form>
+				</div>
 			</div>
 		</div>
-	</div>
-</div>
+	</main>
 
 
 
-<footer>
-	<?php include("includes/footer.html"); ?>
-</footer>
+	<footer>
+		<?php include("includes/footer.html"); ?>
+	</footer>
 
 
 
-<!-- Bootstrap JavaScript -->
-<?php include('includes/bootstrap_javascript.html') ?>
+	<!-- Bootstrap JavaScript -->
+	<?php include('includes/bootstrap_javascript.html') ?>
 
-</body>
+	</body>
 </html>
