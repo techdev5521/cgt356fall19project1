f0c0cba3eaf977acd7d70bb1444b52b97b38f8aa
kmangia <kmangia@purdue.edu>
2019-11-03 22:13:35 -0500

    created signup page
    added link to logo and created signup

--- includes/navigation.php
+++ includes/navigation.php
@@ -2,7 +2,7 @@
 
 <div class="navbar navbar-expand-lg navbar-dark bg-dark box-shadow">
 	<nav class="container d-flex justify-content-between">
-		<a class="navbar-brand" href="#">GameGo</a>
+        <a class="navbar-brand" href="index.php">GameGo</a>
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
 		</button>
