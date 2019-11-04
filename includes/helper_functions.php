f109de3bcca6030862c54c52f77e16c7b9215fe1
Justin Campbell <campbell.justin.tyler@gmail.com>
2019-11-03 22:22:28 -0500

    Added getSessionVariable and printArray

--- includes/helper_functions.php
+++ includes/helper_functions.php
@@ -1,7 +1,23 @@
 <?php
 
-	function helloWorld(){
-		echo('Hello, World!');
+	/** Prints a key's value from $_SESSION if it is not empty.
+	 *
+	 * @param String $key The key to look for in $_SESSION
+	 */
+	function getSessionVariable($key){
+		if (!empty($_SESSION["{$key}"])) {
+			print($_SESSION[$key]);
+		}
+	}
+
+	/** Print's array in human readable format
+	 *
+	 * @param Array $array The array to print
+	 */
+	function printArray($array){
+		foreach ($array as $key => $value) {
+			print("{$key}: $value<br />");
+		}
 	}
 
 ?>
