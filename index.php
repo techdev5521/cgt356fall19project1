7130e0740ab814b816321bf8e4bc77c1a8bd0cc0
Justin Campbell <campbell.justin.tyler@gmail.com>
2019-11-03 20:34:24 -0500

    Completed homepage design

--- index.php
+++ index.php
@@ -25,7 +25,37 @@
 
 
 		<main>
-			
+			<section id="header" class="jumbotron text-center">
+				<div class="container">
+					<h1 class="jumbotron-heading">Welcome to GameGo</h1>
+					<p class="lead text-muted">Playing With Power</p>
+				</div>
+			</section>
+
+			<div class="container">
+				
+				<div class="row my-5 game-banner" id="modern-warfare">
+					<div class="col-md-4 py-5 game-text-overlay">
+						<h2>Call of Duty Modern Warfare</h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, dolor impedit tempore. Molestias rerum voluptatum cumque accusantium ullam consequatur quibusdam!</p>
+					</div>
+				</div>
+
+				<div class="row my-5 game-banner" id="red-dead-redemption-2">
+					<div class="col-md-4 py-5 game-text-overlay">
+						<h2>Red Dead Redemption 2</h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, dolor impedit tempore. Molestias rerum voluptatum cumque accusantium ullam consequatur quibusdam!</p>
+					</div>
+				</div>
+
+				<div class="row my-5 game-banner" id="death-stranding">
+					<div class="col-md-4 py-5 game-text-overlay">
+						<h2>Death Stranding</h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, dolor impedit tempore. Molestias rerum voluptatum cumque accusantium ullam consequatur quibusdam!</p>
+					</div>
+				</div>
+
+			</div>
 		</main>
 
 
