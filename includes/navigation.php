4a113aeb0607431277c4c46513dacf9582f9e532
kmangia <kmangia@purdue.edu>
2019-11-04 21:08:58 -0500

    added readme and updated nav bar

--- includes/navigation.php
+++ includes/navigation.php
@@ -1,28 +1,39 @@
 <!-- Start navigation.html -->
 
 <div class="navbar navbar-expand-lg navbar-dark bg-dark box-shadow">
-	<nav class="container d-flex justify-content-between">
+    <nav class="container d-flex justify-content-between">
         <img class="logo_img" src="images/logo.png" alt="GameGo Logo" onclick="window.location.href = 'index.php'">
         <a class="navbar-brand" href="index.php">GameGo</a>
-		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-			<span class="navbar-toggler-icon"></span>
-		</button>
-		<div class="collapse navbar-collapse" id="navbarSupportedContent">
-			<ul class="navbar-nav ml-auto">
-				<li class="nav-item active">
-					<div class="form-inline my-2 my-lg-0">
-						<?php
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <?php
+                    if (1) { //if logged out
+                        echo("<button class=\"btn btn-light mr-2\" type=\"button\" onclick=\"window.location.href = 'accountInfo.php';\">Account Info</button>");
+                    }else { //If logged in
+                        echo("<button class=\"btn btn-light ml-2\" type=\"button\" onclick=\"window.location.href = 'login.php';\">Account Info</button>}");
+                    }
+                    ?>
+                    <button class="btn btn-light mr-2" type="button" onclick="window.location.href = 'readme.php';">Read Me</button>
+                </li>
+            </ul>
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                    <div class="form-inline my-2 my-lg-0">
+                        <?php
                         if (1) { //if logged out
-							echo("<button class=\"btn btn-light mr-2\" type=\"button\" onclick=\"window.location.href = 'login.php';\">Login</button>");
-							echo("<button class=\"btn btn-light ml-2\" type=\"button\" onclick=\"window.location.href = 'signup.php';\">Sign Up</button>");
-						}else { //If logged in
-                            echo("<button class=\"btn btn-light mr-2\" type=\"button\" onclick=\"window.location.href = '#';\">Account information</button>");
-                            echo("<button class=\"btn btn-light ml-2\" type=\"button\" onclick=\"window.location.href = '#';\">Log out</button>}");
+                            echo("<button class=\"btn btn-light mr-2\" type=\"button\" onclick=\"window.location.href = 'login.php';\">Login</button>");
+                            echo("<button class=\"btn btn-light ml-2\" type=\"button\" onclick=\"window.location.href = 'signup.php';\">Sign Up</button>");
+                        }else { //If logged in
+                            echo("<button class=\"btn btn-light ml-2\" type=\"button\" onclick=\"window.location.href = 'index.php';\">Log out</button>}");
                         }
-						?>
-					</div>
-				</li>
-			</ul>
-	</nav>
+                        ?>
+                    </div>
+                </li>
+            </ul>
+    </nav>
 </div>
 <!-- End navigation.html -->
---
+++ readme.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="em-US">
+
+<head>
+    <title>Project 1: Home</title>
+
+    <!-- Metadata-->
+    <?php include('includes/meta.html'); ?>
+
+    <!-- CSS -->
+    <?php include('includes/css.html'); ?>
+
+    <!-- PHP Libraries -->
+    <?php include('includes/phpLibs.html'); ?>
+
+</head>
+
+<body>
+
+<header>
+    <!-- Navigation -->
+    <?php include('includes/navigation.php'); ?>
+</header>
+
+
+
+<main>
+    <section id="header" class="jumbotron text-center">
+        <div class="container">
+            <h1 class="jumbotron-heading">Welcome to read me</h1>
+            <p class="lead text-muted">We hope you enjoyed our website</p>
+        </div>
+    </section>
+
+    <div class="container">
+        <h4 class="text-center">Website created by: Justin Campbell, Trent Lambrecht, Kai Mangiaracina, & Brad Williams</h4>
+            <p>This is our read me instructions</p>
+            <p>o	Extras (above and beyond) if any</p>
+            <p>o	Explanation of any 'quirks' in your system</p>
+
+
+    </div>
+</main>
+
+
+
+<footer>
+    <?php include("includes/footer.html"); ?>
+</footer>
+
+
+
+<!-- Bootstrap JavaScript -->
+<?php include('includes/bootstrap_javascript.html') ?>
+
+<!-- JS Libraries -->
+<?php include('includes/jsLibs.html'); ?>
+
+</body>
+
+
+</html>
