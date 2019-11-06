9ec3e93733a4dd6eafdd0a3455ce97554d56f0d1
Justin Campbell <campbell.justin.tyler@gmail.com>
2019-11-06 12:49:16 -0500

    Moved readme to use card decks

--- readme.php
+++ readme.php
@@ -35,72 +35,67 @@
 
 			<div class="container pb-5" id="readme">
 				<div class="row justify-content-center">
-					
-						<div class="col-xs-3">
-							<div class="card box-shadow mb-3">
-								<div class="card-header text-center">
-									<h5>Trent Lambrecht</h5>
-								</div>
-								<img class="card-img-top" src="images/trent.png" alt="Trent Lambrecht">
-								<div class="card-body">
-									<h5 class="card-title">That red headed kid</h5>
-									<p class="card-text">"Failure is always an option"</p>
-								</div>
-								<ul class="list-group list-group-flush">
-									<li class="list-group-item"><strong>Major: </strong> VPI and Web Design</li>
-								</ul>
+
+					<div class="card-deck">
+						<div class="card box-shadow mb-3">
+							<div class="card-header text-center">
+								<h5>Trent Lambrecht</h5>
 							</div>
+							<img class="card-img-top" src="images/trent.png" alt="Trent Lambrecht">
+							<div class="card-body">
+								<h5 class="card-title">That red headed kid</h5>
+								<p class="card-text">"Failure is always an option"</p>
+							</div>
+							<ul class="list-group list-group-flush">
+								<li class="list-group-item"><strong>Major: </strong> VPI and Web Design</li>
+							</ul>
 						</div>
 						
-						<div class="col-xs-3">
-							<div class="card box-shadow mb-3">
-								<div class="card-header text-center">
-									<h5>Kai Mangiaracina</h5>
-								</div>
-								<img class="card-img-top" src="images/kai.PNG" alt="Kai Mangiaracina">
-								<div class="card-body">
-									<h5 class="card-title">"MangoTree"</h5>
-									<p class="card-text">Behind every good programmer there is a frustrated programmer.</p>
-								</div>
-								<ul class="list-group list-group-flush">
-									<li class="list-group-item"><strong>Major: </strong> Mechanical Engineering Technology & Virtual Product Integration</li>
-								</ul>
+						<div class="card box-shadow mb-3">
+							<div class="card-header text-center">
+								<h5>Kai Mangiaracina</h5>
 							</div>
+							<img class="card-img-top" src="images/kai.PNG" alt="Kai Mangiaracina">
+							<div class="card-body">
+								<h5 class="card-title">"MangoTree"</h5>
+								<p class="card-text">Behind every good programmer there is a frustrated programmer.</p>
+							</div>
+							<ul class="list-group list-group-flush">
+								<li class="list-group-item"><strong>Major: </strong> Mechanical Engineering Technology & Virtual Product Integration</li>
+							</ul>
 						</div>
 
-						<div class="col-xs-3">
-							<div class="card box-shadow mb-3">
-								<div class="card-header text-center">
-									<h5>Justin Campbell</h5>
-								</div>
-								<img class="card-img-top" src="images/justin.png" alt="Justin Campbell">
-								<div class="card-body">
-									<h5 class="card-title">Mom</h5>
-									<p class="card-text">SHORT QUOTE</p>
-								</div>
-								<ul class="list-group list-group-flush">
-									<li class="list-group-item"><strong>Major: </strong> Web Design</li>
-								</ul>
+						<div class="card box-shadow mb-3">
+							<div class="card-header text-center">
+								<h5>Justin Campbell</h5>
 							</div>
+							<img class="card-img-top" src="images/justin.png" alt="Justin Campbell">
+							<div class="card-body">
+								<h5 class="card-title">Mom</h5>
+								<p class="card-text">SHORT QUOTE</p>
+							</div>
+							<ul class="list-group list-group-flush">
+								<li class="list-group-item"><strong>Major: </strong> Web Design</li>
+							</ul>
 						</div>
 
-						<div class="col-xs-3">
-							<div class="card box-shadow mb-3">
-								<div class="card-header text-center">
-									<h5>Brad Williams</h5>
-								</div>
-								<img class="card-img-top" src="images/brad.png" alt="Brad Williams">
-								<div class="card-body">
-									<h5 class="card-title">Doc</h5>
-									<p class="card-text">SHORT QUOTE</p>
-								</div>
-								<ul class="list-group list-group-flush">
-									<li class="list-group-item"><strong>Major: </strong> Web Design</li>
-								</ul>
+						<div class="card box-shadow mb-3">
+							<div class="card-header text-center">
+								<h5>Brad Williams</h5>
 							</div>
+							<img class="card-img-top" src="images/brad.png" alt="Brad Williams">
+							<div class="card-body">
+								<h5 class="card-title">Doc</h5>
+								<p class="card-text">SHORT QUOTE</p>
+							</div>
+							<ul class="list-group list-group-flush">
+								<li class="list-group-item"><strong>Major: </strong> Web Design</li>
+							</ul>
 						</div>
 					</div>
+
 				</div>
+			</div>
 
 				<p>Extras (above and beyond) if any</p>
 				<p>Explanation of any 'quirks' in your system</p>
