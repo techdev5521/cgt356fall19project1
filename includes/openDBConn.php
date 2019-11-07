cced89661ba80790acd30c2d6a59048fba928fe1
Justin Campbell <justincampbell@Justins-MacBook-Pro-2.local>
2019-11-07 18:56:50 -0500

    Modified database user for server

--- includes/openDBConn.php
+++ includes/openDBConn.php
@@ -1,8 +1,8 @@
 <?php
 
 $db_host = "localhost";
-$db_user = "root";
-$db_pass = "TechDev#5521";
+$db_user = "project1data";
+$db_pass = "VyGVFnf$7bY5XTj";
 $db_name = "gamego";
 
 $conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
