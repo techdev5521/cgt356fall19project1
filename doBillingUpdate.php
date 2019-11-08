6dd2590781f3a9a783bffbd13170663d2be702c9
Justin Campbell <justincampbell@Justins-MacBook-Pro-2.local>
2019-11-08 02:01:17 -0500

    Create doBillingUpdate.php

---
+++ doBillingUpdate.php
@@ -0,0 +1,43 @@
+<?php 
+
+	// Start Sesson
+	session_start();
+
+	// Unset Error Message
+	unset($_SESSION["errorMessage"]);
+
+	// Include phpLibs
+	include("includes/phpLibs.html");
+
+	// Open Database Connection
+	include("includes/openDBConn.php");
+
+	// Sanitize inputs using mysqli::real_escape_string
+	foreach ($_POST as $key => $value) {
+		$_POST[$key] = $conn->real_escape_string($value);
+	}
+
+	// Perform Update
+	$sql = "UPDATE `Billing` SET ";
+	$sql = $sql . "`cardType` = '" . $_POST['cardType'] . "'";
+	$sql = $sql . ", `cardExpirationMonth` = '" . $_POST['cardExpirationMonth'] . "'";
+	$sql = $sql . ", `cardExpirationYear` = '" . $_POST['cardExpirationYear'] . "'";
+	$sql = $sql . ", `billingFirstName` = '" . $_POST['billingFirstName'] . "'";
+	$sql = $sql . ", `billingStreet` = '" . $_POST['billingStreet'] . "'";
+	$sql = $sql . ", `billingCity` = '" . $_POST['billingCity'] . "'";
+	$sql = $sql . ", `billingState` = '" . $_POST['billingState'] . "'";
+	$sql = $sql . ", `billingZip` = '" . $_POST['billingZip'] . "'";
+	$sql = $sql . " WHERE `Billing`.`cardNumber` = " . $_POST['cardNumber'] . ";";
+
+	$result = $conn->query($sql);
+
+	// CLose Database Connection
+	include("includes/closeDBConn.php");
+
+	// Set success message
+	setSessionVariable("successMessage", "You billing information was updated successfully.");
+
+	// Redirect to Account Information
+	header("Location:accountInfo.php");
+
+?>
